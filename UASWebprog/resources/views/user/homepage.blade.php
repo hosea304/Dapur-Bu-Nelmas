8aeefc19a38f7a166797acd72657beadc8ee6f33
BenJrx <128213708+BenJrx@users.noreply.github.com>
2023-12-05 00:13:01 +0700

    responsive & styling

--- UASWebprog/resources/views/user/homepage.blade.php
+++ UASWebprog/resources/views/user/homepage.blade.php
@@ -6,121 +6,161 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beranda</title>
     <link rel="stylesheet" href="{{ asset('../resources/views/user/styles.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <style>
+
         body {
-    font-family: 'Arial', sans-serif;
-    margin: 0;
-    padding: 0;
-}
+            background-image: url('{{ asset("user/asset gambar/backgroundweb.jpg") }}');
+            background-size: cover;  
+            font-family: monospace;
+            font-weight: bold;
+            padding: 0;
+        }
 
-header {
-    background-color: #fff;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    position: relative;
-    z-index: 2;
-}
+        header {
+            background-color: #fff;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            position: relative;
+            z-index: 2;
+        }
 
-.navbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px;
-}
+        .navbar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px;
+        }
 
-.sub-navbar {
-    display: flex;
-    justify-content: center;
-    background-color: #eee;
-}
+        .sub-navbar {
+            display: flex;
+            justify-content: center;
+            background-color: #eee;
+        }
 
-.nav-option {
-    padding: 10px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-}
+        .nav-option {
+            padding: 10px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
 
-.nav-option:hover {
-    background-color: #ddd;
-}
+        .nav-option:hover {
+            background-color: #ddd;
+        }
+        .showcase {
+        width: 100%;
+        height: 500px;
+        background-image: url('{{ asset("user/asset gambar/showcase home.gif") }}');
+        background-size: 100% 100%;
+        background-position: center center;
+        background-repeat: no-repeat;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        margin: 20px 0;
+    }
 
-.showcase {
-    width: 100%;
-    height: 500px;
-    background-image: url('{{ asset("user/asset gambar/showcase home.gif") }}');
-    background-size: 100% 100%; 
-    background-position: center center;
-    background-repeat: no-repeat;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    margin: 20px 0;
-}
+    @media (max-width: 1500px) {
 
+        .showcase {
+            height: 250px; 
+            margin: 10px 0; 
+        }
+    }
+        hr {
+            border: 1px solid #ccc;
+        }
 
-hr {
-    border: 1px solid #ccc;
-}
+        .category-container {
+            display: flex;
+            justify-content: space-around;
+            margin: 20px 0;
+        }
 
-.category-container {
-    display: flex;
-    justify-content: space-around;
-    margin: 20px 0;
-}
+        .category-box {
+            width: 100%;
+            max-width: 350px;
+            height: 200px;
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            transition: transform 0.3s;
+            margin-bottom: 25px;
+            background-color: white;
+        }
 
-.category-box {
-    width: 350px;
-    height: 200px;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    transition: transform 0.3s;
-}
+        .category-box img {
+            margin-top: 11px;
+            width: 200px
+            object-fit: cover;
+            border-radius: 10px; 
 
-.category-box:hover {
-    transform: scale(1.05);
-}
+        }
 
-.list-container {
-    display: flex;
-    justify-content: space-around;
-    margin: 20px 0;
-}
+        .category-box:hover {
+            transform: scale(1.05);
+        }
 
-.list-box {
-    width: 200px;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    transition: transform 0.3s;
-}
+        .list-container {
+            display: flex;
+            justify-content: space-around;
+            margin: 20px 0;
+        }
 
-.list-box:hover {
-    transform: scale(1.05);
-}
-</style>
+        .list-box {
+            width: 100%;
+            max-width: 200px;
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+            transition: transform 0.3s;
+        }
+
+        .list-box img {
+            width: 100%;
+            height: auto;
+            border-radius: 10px 10px 0 0;
+        }
+
+        .list-box:hover {
+            transform: scale(1.05);
+        }
+
+        .text-center {
+            font-family: monospace;
+            font-weight: bold;
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
     <header>
-    <nav class="navbar">
-    <div class="left-icons">
-        <span class="cart-icon" onclick="openCart()">
-            <img src="{{ asset('user/asset gambar/item1.jpg') }}" alt="Keranjang">
-        </span>
-    </div>
-    <div class="logo">
-    <div style="text-align: center;">
-    <img src="{{ asset('user/asset gambar/logo usaha.png') }}" alt="Logo Usaha" width="80" height="80" style="display: inline-block;">
-</div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('user/asset gambar/logo usaha.png') }}" alt="Logo Usaha" width="80" height="80">
+                <span>DAPUR BU NELMAS</span>
+            </a>
 
-        <p>DAPUR BU NELMAS</p>
-    </div>
-    <div class="right-icons">
-        <span class="user-icon" onclick="openAccount()">
-            <img src="{{ asset('user/assets/user-icon.png') }}" alt="Pengguna">
-        </span>
-    </div>
-</nav>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="openCart()">
+                            <img src="{{ asset('user/asset gambar/shoppingcart.png') }}" alt="Keranjang" width="30" height="30">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="openAccount()">
+                            <img src="{{ asset('user/asset gambar/usericon.png') }}" alt="Pengguna" width="30" height="30">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <div class="sub-navbar">
             <div class="nav-option" onclick="scrollToSection('home')">BERANDA</div>
             <div class="nav-option" onclick="scrollToSection('products')">PRODUK</div>
@@ -133,41 +173,44 @@ hr {
 
     <hr>
 
-    <section id="products">
-    <h2 style="text-align: center;">KATEGORI MAKANAN</h2>
-        <div class="category-container">
-            <div class="category-box" onclick="showCategory('category1')">
-                <img src="{{ asset('user/asset gambar/kue showcase.jpg') }}" alt="Kategori 1" width="350" height="200">
+    <section id="products" class="container">
+        <h2 class="text-center">KATEGORI MAKANAN</h2>
+        <div class="category-container row">
+            <div class="category-box col-md-4" onclick="showCategory('category1')">
+                <img src="{{ asset('user/asset gambar/kue showcase.jpg') }}" alt="Kategori 1" class="img-fluid">
             </div>
-            <div class="category-box" onclick="showCategory('category2')">
-            <img src="{{ asset('user/asset gambar/nasi bento showcase.jpg') }}" alt="Kategori 2" width="350" height="200">
+            <div class="category-box col-md-4" onclick="showCategory('category2')">
+                <img src="{{ asset('user/asset gambar/nasi bento showcase.jpg') }}" alt="Kategori 2" class="img-fluid">
             </div>
-            <div class="category-box" onclick="showCategory('category3')">
-            <img src="{{ asset('user/asset gambar/ricebowl showcase.jpg') }}" alt="Kategori 3" width="350" height="200">
+            <div class="category-box col-md-4" onclick="showCategory('category3')">
+                <img src="{{ asset('user/asset gambar/ricebowl showcase.jpg') }}" alt="Kategori 3" class="img-fluid">
             </div>
         </div>
     </section>
 
     <hr>
 
-    <section id="list">
-    <h2 style="text-align: center;">LIST HARI INI</h2>
-        <div class="list-container">
-            <div class="list-box" onclick="showList('item1')">
-                <img src="{{ asset('user/assets/item1.jpg') }}" alt="Item 1">
-                <p>Nama Item 1</p>
+    <section id="list" class="container">
+        <h2 class="text-center">LIST HARI INI</h2>
+        <div class="list-container row">
+            <div class="list-box col-md-4" onclick="showList('item1')">
+                <img src="{{ asset('user/assets/item1.jpg') }}" alt="Item 1" class="img-fluid">
+                <p class="text-center">Nama Item 1</p>
             </div>
-            <div class="list-box" onclick="showList('item2')">
-                <img src="{{ asset('user/assets/item2.jpg') }}" alt="Item 2">
-                <p>Nama Item 2</p>
+            <div class="list-box col-md-4" onclick="showList('item2')">
+                <img src="{{ asset('user/assets/item2.jpg') }}" alt="Item 2" class="img-fluid">
+                <p class="text-center">Nama Item 2</p>
             </div>
-            <div class="list-box" onclick="showList('item3')">
-                <img src="{{ asset('user/assets/item3.jpg') }}" alt="Item 3">
-                <p>Nama Item 3</p>
+            <div class="list-box col-md-4" onclick="showList('item3')">
+                <img src="{{ asset('user/assets/item3.jpg') }}" alt="Item 3" class="img-fluid">
+                <p class="text-center">Nama Item 3</p>
             </div>
         </div>
     </section>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="{{ asset('user/scripts.js') }}"></script>
 </body>
 
--- UASWebprog/resources/views/user/product.blade.php
+++ UASWebprog/resources/views/user/product.blade.php
@@ -6,10 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produk</title>
     <link rel="stylesheet" href="{{ asset('../resources/views/user/styles.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <style>
         body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
+            background-image: url('{{ asset("user/asset gambar/backgroundweb.jpg") }}');
+            background-size: cover;  
+            font-family: monospace;
+            font-weight: bold;
             padding: 0;
         }
 
@@ -47,9 +50,10 @@
             margin: 20px 0;
             padding: 20px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            background-color: #fff;
-            align-items: center;
+            background-color: #9EB8D9;
             text-align: center;
+            border-radius: 10px; 
+
         }
 
         .section-title {
@@ -62,12 +66,12 @@
             text-align: center;
             transition: transform 0.3s ease-in-out;
             display: inline-block;
-            width: 250px;
+            width: 100%; 
+            max-width: 250px; 
             box-sizing: border-box;
-            margin-right: 130px; /* Adjust the margin between items horizontally */
-            margin-bottom: 20px; /* Adjust the margin between items vertically */
-            margin-left: 110px;
+            margin-bottom: 20px; 
         }
+
         .menu-item:hover {
             transform: scale(1.05);
         }
@@ -79,35 +83,40 @@
             height: auto;
         }
 
-        /* Additional styles for separating sections */
         hr {
             margin: 20px 0;
             border: 0;
             border-top: 1px solid #eee;
         }
-
-
     </style>
 </head>
 
 <body>
     <header>
-        <nav class="navbar">
-            <div class="left-icons">
-                <span class="cart-icon" onclick="openCart()">
-                    <img src="{{ asset('user/asset gambar/item1.jpg') }}" alt="Keranjang">
-                </span>
-            </div>
-            <div class="logo">
-                <div style="text-align: center;">
-                    <img src="{{ asset('user/asset gambar/logo usaha.png') }}" alt="Logo Usaha" width="80" height="80" style="display: inline-block;">
-                </div>
-                <p>DAPUR BU NELMAS</p>
-            </div>
-            <div class="right-icons">
-                <span class="user-icon" onclick="openAccount()">
-                    <img src="{{ asset('user/assets/user-icon.png') }}" alt="Pengguna">
-                </span>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('user/asset gambar/logo usaha.png') }}" alt="Logo Usaha" width="80" height="80">
+                <span>DAPUR BU NELMAS</span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="openCart()">
+                            <img src="{{ asset('user/asset gambar/shoppingcart.png') }}" alt="Keranjang" width="30" height="30">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" onclick="openAccount()">
+                            <img src="{{ asset('user/asset gambar/usericon.png') }}" alt="Pengguna" width="30" height="30">
+                        </a>
+                    </li>
+                </ul>
             </div>
         </nav>
         <div class="sub-navbar">
@@ -118,88 +127,100 @@
         </div>
     </header>
     <br />
-    <h2 style="text-align: center;">PRODUK</h2>
-    <div class="section-container">
-        <div class="section-title">KUE</div>
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/pie1.jpg') }}" alt="Menu 1">
-            <p>MINI PIE SUSU</p>
-        </div>
+    <h2 style="text-align: center; font-family: monospace; font-weight: bold; color: white;">PRODUK</h2>
+    <div class="container">
+        <div class="section-container">
+            <div class="section-title">KUE</div>
+            <div class="row justify-content-center">
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/pie1.jpg') }}" alt="Menu 1">
+                    <p>MINI PIE SUSU</p>
+                </div>
 
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/pie2.jpg') }}" alt="Menu 2">
-            <p>MINI PIE COKELAT</p>
-        </div>
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/pie2.jpg') }}" alt="Menu 2">
+                    <p>MINI PIE COKELAT</p>
+                </div>
 
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/pie3.jpg') }}" alt="Menu 3">
-            <p>MINI PIE BUAH</p>
-        </div>
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/pie3.jpg') }}" alt="Menu 3">
+                    <p>MINI PIE BUAH</p>
+                </div>
 
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/pie4.jpg') }}" alt="Menu 4">
-            <p>MINI PIE NANAS</p>
-        </div>
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/pie4.jpg') }}" alt="Menu 4">
+                    <p>MINI PIE NANAS</p>
+                </div>
 
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/risolmayo.jpg') }}" alt="Menu 5">
-            <p>RISOL MAYO</p>
-        </div>
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/risolmayo.jpg') }}" alt="Menu 5">
+                    <p>RISOL MAYO</p>
+                </div>
 
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/pizza.jpg') }}" alt="Menu 6">
-            <p>MINI PIZZA</p>
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/pizza.jpg') }}" alt="Menu 6">
+                    <p>MINI PIZZA</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+        <div class="section-container">
+            <div class="section-title">PAKET NASI BENTO</div>
+            <div class="row justify-content-center">
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/bento1.jpg') }}" alt="Menu 7">
+                    <p>BENTO (-)</p>
+                </div>
+
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/bento2.jpg') }}" alt="Menu 8">
+                    <p>BENTO (-)</p>
+                </div>
+
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/bento3.jpg') }}" alt="Menu 9">
+                    <p>BENTO (-)</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+        <div class="section-container">
+            <div class="section-title">PAKET NASI RICE BOWL</div>
+            <div class="row justify-content-center">
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/ricebowl1.jpg') }}" alt="Menu 10">
+                    <p>-</p>
+                </div>
+
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/ricebowl2.jpg') }}" alt="Menu 11">
+                    <p>-</p>
+                </div>
+
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/ricebowl3.jpg') }}" alt="Menu 12">
+                    <p>-</p>
+                </div>
+
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/ricebowl4.jpg') }}" alt="Menu 13">
+                    <p>-</p>
+                </div>
+
+                <div class="menu-item col-md-4">
+                    <img src="{{ asset('user/asset gambar/ricebowl5.jpg') }}" alt="Menu 14">
+                    <p>-</p>
+                </div>
+            </div>
         </div>
     </div>
-    <hr>
 
-    <div class="section-container">
-        <div class="section-title">PAKET NASI BENTO</div>
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/bento1.jpg') }}" alt="Menu 4">
-            <p>BENTO (BLOM TAU NAMANYA)</p>
-        </div>
-
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/bento2.jpg') }}" alt="Menu 5">
-            <p>BENTO (BLOM TAU NAMANYA)</p>
-        </div>
-
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/bento3.jpg') }}" alt="Menu 6">
-            <p>BENTO (BLOM TAU NAMANYA)</p>
-        </div>
-    </div>
-
-    <hr>
-
-    <div class="section-container">
-        <div class="section-title">PAKET NASI RICE BOWL</div>
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/ricebowl1.jpg')}}" alt="Menu 7">
-            <p>BLOM TAU NAMANYA</p>
-        </div>
-
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/ricebowl2.jpg') }}" alt="Menu 8">
-            <p>BLOM TAU NAMANYA</p>
-        </div>
-
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/ricebowl3.jpg') }}" alt="Menu 9">
-            <p>BLOM TAU NAMANYA</p>
-        </div>
-
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/ricebowl4.jpg') }}" alt="Menu 9">
-            <p>BLOM TAU NAMANYA</p>
-        </div>
-
-        <div class="menu-item">
-            <img src="{{ asset('user/asset gambar/ricebowl5.jpg') }}" alt="Menu 9">
-            <p>BLOM TAU NAMANYA</p>
-        </div>
-    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="{{ asset('user/scripts.js') }}"></script>
 </body>
 
 </html>
