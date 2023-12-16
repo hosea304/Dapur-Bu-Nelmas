7921ffa49deeb2dabe4a978aacd2370a4fa52209
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-12-16 11:15:46 +0700

    Update infopesanan.blade.php

--- UASWebprog/resources/views/user/infopesanan.blade.php
+++ UASWebprog/resources/views/user/infopesanan.blade.php
@@ -7,25 +7,25 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('user/user-style.css') }}">
 </head>
-<body class="aboutus" style="background-image: url('user/asset gambar/backgroundweb.jpg'); background-size: cover;">
+<body class="aboutus" style="background-image: url('user/asset gambar/backgroundweb.jpg'); background-size: cover; color: white;">
     @include('user.navbar')
     <br><br><br><br><br>
     <div class="container mt-5">
-        <h2>Info Pesanan</h2>
+        <h2 style="color: white;">Info Pesanan</h2>
         <table class="table">
             <thead>
                 <tr>
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>Status</th>
+                    <th style="color: white;">No</th>
+                    <th style="color: white;">Nama</th>
+                    <th style="color: white;">Status</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($orders as $index => $order)
                     <tr>
-                        <td>{{ $index + 1 }}</td>
-                        <td>{{ $order->name }}</td>
-                        <td>{{ $order->status }}</td>
+                        <td style="color: white;">{{ $index + 1 }}</td>
+                        <td style="color: white;">{{ $order->name }}</td>
+                        <td style="color: white;">{{ $order->status }}</td>
                     </tr>
                 @endforeach
             </tbody>
