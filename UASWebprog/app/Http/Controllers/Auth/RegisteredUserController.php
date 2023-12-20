2f7863e550b5e22dba0bacb1a1d87c6be52eebc8
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-12-20 22:06:18 +0700

    nambah alamat di regis

--- UASWebprog/app/Http/Controllers/Auth/RegisteredUserController.php
+++ UASWebprog/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,13 +32,15 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'alamat' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'alamat' => $request->alamat,
             'password' => Hash::make($request->password),
         ]);
 
