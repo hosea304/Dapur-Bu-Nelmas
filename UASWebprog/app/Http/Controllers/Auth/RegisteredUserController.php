dab7bbe59faa740d97e2b17c260be8b6c2413252
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-12-21 15:37:37 +0700

    no telp

--- UASWebprog/app/Http/Controllers/Auth/RegisteredUserController.php
+++ UASWebprog/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'alamat' => ['required', 'string', 'max:255'],
-            'noTelp' => ['required', 'integer'],
+            'noTelp' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
--- UASWebprog/database/migrations/2023_12_10_033224_users.php
+++ UASWebprog/database/migrations/2023_12_10_033224_users.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('alamat');
-            $table->integer('noTelp')->unsigned();
+            $table->string('noTelp');
             $table->string('password');
             $table->string('usertype')->default('user');
             $table->timestamps();
